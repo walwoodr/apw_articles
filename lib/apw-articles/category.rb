56a71cadd9777a13113e0b0317fbc0d96d14545d
walwoodr <walwoodr@gmail.com>
2017-03-20 09:02:19 -0500

    Beginning to scrape categories and create category objects. Not quite working yet.

--- lib/apw-articles/category.rb
+++ lib/apw-articles/category.rb
@@ -1,14 +1,23 @@
 class APWArticles::Category
-  attr_accessor :name
+  attr_accessor :name, :articles
   @@all = []
 
   def initialize(name = nil)
     self.name = name
     self.class.all << self
+    self.articles = []
   end
 
   def self.all
     @@all
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.all.detect{|category| category.name == name } == nil
+      self.new(name)
+    else
+
+    end
   end
 
 end
--- lib/apw-articles/scraper.rb
+++ lib/apw-articles/scraper.rb
@@ -17,15 +17,18 @@ class APWArticles::Scraper
 
   def self.scrape_categories
     doc = Nokogiri::HTML(open("https://apracticalwedding.com/category/marriage-essays/?listas=list")).css(".type-post")
-    link_classes = []
-    doc.each { |link| link_classes << link.attribute("class").value }
+    link_attributes = []
+    doc.each { |link| link_attributes << link.attribute("class").value }
     link_attributes.each do |attributes_list|
-      attributes_array = attributes_list.split(/ category-/).slice!(0)
+      attributes_array = attributes_list.split(/ category-/)
+      attributes_array.slice!(0)
       attributes_array.each do |category|
-        
-      end
-    end
-  end # I want this to return an array of categories on articles on a generic list
+        APWArticles::Category.find_or_create_by_name(category)
+      end # attributes_array do end
+    end # link_attributes do end
+    APWArticles::Category.all
+    binding.pry
+  end # self.scrape_categories end
 
 end
 
