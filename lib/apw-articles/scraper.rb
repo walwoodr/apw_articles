5d73620967d3b46248884087fe6d0d1b75c48f74
walwoodr <walwoodr@gmail.com>
2017-03-20 08:40:26 -0500

    Add logic to create category array per article

--- lib/apw-articles/scraper.rb
+++ lib/apw-articles/scraper.rb
@@ -1,11 +1,12 @@
 require 'nokogiri'
 require 'open-uri'
 require 'pry'
+require_relative '../apw-articles.rb'
 
 class APWArticles::Scraper
 
   def self.scrape_list(url, category)
-
+      # URL example: https://apracticalwedding.com/category/marriage-essays/kids-no-kids/page/2/?listas=list
   end # I want this to return a hash of articles in a given category
 
   def self.scrape_article(url)
@@ -14,16 +15,18 @@ class APWArticles::Scraper
 
   private
 
-  def self.scrape_categories(url)
-
-  end # I want this to return a hash of categories on articles on a generic list
+  def self.scrape_categories
+    doc = Nokogiri::HTML(open("https://apracticalwedding.com/category/marriage-essays/?listas=list")).css(".type-post")
+    link_classes = []
+    doc.each { |link| link_classes << link.attribute("class").value }
+    link_attributes.each do |attributes_list|
+      attributes_array = attributes_list.split(/ category-/).slice!(0)
+      attributes_array.each do |category|
+        
+      end
+    end
+  end # I want this to return an array of categories on articles on a generic list
 
 end
 
-
-# i=1
-# doc = {}
-# until i == 13 # the actual list goes up to 12 pages
-#   doc["Page #{i}"] =  Nokogiri::HTML(open("https://apracticalwedding.com/category/marriage-essays/page/#{i}/?listas=list"))
-#   i+=1
-# end
+APWArticles::Scraper.scrape_categories
