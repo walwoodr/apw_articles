752488db2f302003a9193a78a616b0831f746206
walwoodr <walwoodr@gmail.com>
2017-03-20 11:14:40 -0500

    Modify article initialize to accept hash.

--- lib/apw-articles/article.rb
+++ lib/apw-articles/article.rb
@@ -2,20 +2,22 @@ class APWArticles::APWArticles
   attr_accessor :title, :author, :blurb, :url, :categories
   @@all = []
 
-  def initialize(title, blurb, url, categories, author = nil) #does this need to be an info hash? I don't think so -- I know what each article has. But author should probably be optional?
-    self.title = title
-    self.author = author unless author == nil
-    self.blurb = "#{blurb}..."
-    self.url = url
+  def initialize(attribute_hash)
     self.categories = []
-    categories.each do |category|
-      self.categories << APWArticles::Category.find_or_create_by_url # this depends on find_or_create_by_url returning an object
-      # can i actually do a many:many relationship?
-    end
+    attribute_hash.each do |key, value|
+      if key == :categories
+        values.each do |category|
+          self.categories << APWArticles::Category.find_or_create_by_url(category)
+          # NOTE: this depends on find_or_create_by_url returning an object
+          # NOTE: this is a many:many relationship--may cause problems
+        end
+      else
+        self.send(("#{key}="), value)
+      end
   end
 
   def self.new_from_url(url)
-    # call scraper and then initialize from hash
+    
   end
 
   def self.new_from_list(list_url)
